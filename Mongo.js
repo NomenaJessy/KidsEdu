0e429bcbfaf1bed0aadd109e889b9a2dd8d1bc88
NomenaJessy <nomenajessy.nj@gmail.com>
2022-05-19 19:02:33 +0300

    update

--- Mongo.js
+++ Mongo.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/kidsedu",{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+mongoose.connect("mongodb+srv://kidsedu:kids123456@cluster0.jrivq.mongodb.net/kidsedu",{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
     console.log('database is connected successfully');
 }).catch(error =>{
     console.log('database not connected due to '+ error);
